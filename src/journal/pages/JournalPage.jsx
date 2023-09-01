d24b3a783de2ac76b06669e44a235be8ab7bdbb1
Agustin <zagoagus@gmail.com>
2023-09-01 18:50:33 -0300

    Design of the app

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material'
+import { JournalLayout } from '../layout/JournalLayout'
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+      <Typography variant='h5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet magnam numquam molestiae libero aliquid aperiam praesentium incidunt beatae nemo dolor dolorem laudantium, alias ipsa recusandae illum, vitae voluptas? Repellat, asperiores?</Typography>
+    </JournalLayout>
+  )
+}
